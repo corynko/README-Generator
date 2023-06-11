403169e43f44e2243a3e21965ad4561af2ebb926
Ethan <131805068+corynko@users.noreply.github.com>
2023-06-11 14:37:06 -0600

    Created README template markdown, begin implementation of inquirer and fs

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // TODO: Create an array of questions for user input
+inquirer;
 const questions = [];
 
+//use github api to pull repo name from repo url?
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const rmTemplate = `
+# ${projectTitle}
+
+${elevPitch}
+
+## Description
+
+${whatIsIt}
+
+# Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Screenshots](#screenshots)
+4. [License](#license)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+
+    * ${instruction1}
+    * ${instruction2}
+    * ${instruction3}
+
+## Usage
+
+${deployedUrl}
+
+## Screenshots
+
+    ![${altText1}](./assets/images/image_1)
+    ![${altText2}](./assets/images/image_2)
+
+## License
+
+${licenseText}
+
+## Contributors
+
+This project was built by ${credits}.
+
+## Tests
+
+## Questions
+
+Please don't hesitate to reach out to ${gitUser} or open an issue on the project repository with any questions or bugs.
+
+- made using @corynko's readme generator -`;
+
+fs.writeFile("Generated_README.md", rmTemplate, (err) => {
+  if (err) console.log("readme err");
+});
 
 // TODO: Create a function to initialize app
 function init() {}
---
+++ test.md
@@ -0,0 +1,47 @@
+# ${projectTitle}
+
+$elevPitch
+
+## Description
+
+$whatIsIt
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Screenshots](#screenshots)
+4. [License](#license)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+
+    * $instruction1
+    * $instruction2
+    * $instruction3
+
+## Usage
+
+$deployedUrl
+
+## Screenshots
+
+    !$altText1(./assets/images/image_1)
+    !altText2(./assets/images/image_2)
+
+## License
+
+$licenseText
+
+## Contributors
+
+This project was built by $credits.
+
+## Tests
+
+## Questions
+
+Please don't hesitate to reach out to $gitUser or open an issue on the project repository with any questions or bugs.
+
+- made using @corynko's readme generator -
